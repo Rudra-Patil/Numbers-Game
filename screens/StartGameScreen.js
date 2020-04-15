222dfc948c59bfb3d9f51fff419b60b74cb1d3be
Rudra Patil <rudrapatil1@gmail.com>
2020-04-15 10:52:25 +0530

    Added InputHandler conditions

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,21 +1,61 @@
-import React from 'react';
-import {View,Text,StyleSheet,TextInput,Button}  from 'react-native';
+import React, { useState } from 'react';
+import {View,Text,StyleSheet,Button,TouchableWithoutFeedback,Keyboard,Alert}  from 'react-native';
 
 import Card from '../Components/Card';
+import colors from '../constants/colors';
+import Input from '../Components/Input';
+
 
 const StartGameScreen = props => {
+
+    const[enteredValue,setEnteredValue]=useState('');
+    const[confirmed,setConfirmed]=useState(false);
+    const[selectedNumber,setSelectedNumber]=useState();
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g,' '));
+    };
+
+    const resetInputHandler = () => {
+        setEnteredValue('');
+        setConfirmed(false);
+    };
+
+    const confirmInputHandler = () =>{
+        const chosenNumber = (parseInt(enteredValue))
+        if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99){
+            Alert.alert('Invalid Number!','Number has to be a number between 1 and 99.',[{text:'Okay',style:'destructive',onPress:resetInputHandler}])
+            return;
+        }
+
+        setConfirmed(true);
+        setEnteredValue('');
+        setSelectedNumber(chosenNumber);
+    };
+
+    let confirmedOutput;
+    
+    if (confirmed){
+    confirmedOutput = <Text>Chosen Number:{selectedNumber}</Text>
+    }
+
     return(
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+        }}>
         <View style={styles.screen}>
             <Text style={style=styles.title}>Start a New Game!</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number!</Text>
-                <TextInput/>
+                <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType='numeric' maxLength={2} onChangeText={numberInputHandler} value={enteredValue}/>
                 <View style={styles.buttonContainer}>
-                    <Button title="Reset" onPress={() => {}}/>
-                    <Button title="Confirm" onPress={() => {}}/>
+                    <View style={styles.button}><Button title="Reset" onPress={resetInputHandler} color={colors.accent}/></View>
+                    <View style={styles.button}><Button title="Confirm" onPress={confirmInputHandler} color={colors.primary}/></View>
                 </View>
             </Card>
+            {confirmedOutput}
         </View>
+        </TouchableWithoutFeedback>
     );
 };
 //adding another view in order to place the buttons in a sideways manner and not above one another
@@ -40,7 +80,12 @@ const styles = StyleSheet.create({
         width:'100%',
         justifyContent:'space-between',
         paddingHorizontal:15
-
+    },
+    button:{
+        width:100
+    },
+    input:{
+        width:40
     }
 
 });

7eccbc063159e8c5f3401a2abde4919a8d437efd
Rudra Patil <rudrapatil1@gmail.com>
2020-04-21 22:41:50 +0530

    Added gameover props and changes

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {View,StyleSheet,Text} from 'react-native';
+import {View,StyleSheet,Text,Button} from 'react-native';
 
 const GameOverScreen = props => {
     return(
         <View style={styles.screen}>
             <Text>The Game is Over!</Text>
+            <Text>Number of Rounds:{props.roundsNumber}</Text>
+            <Text>The Number was:{props.userNumber}</Text>
+            <Button title="NEW GAME" onPress={props.onRestart}/>
         </View>
     )
 };
